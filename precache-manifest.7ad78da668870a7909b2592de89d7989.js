086c9200aaa876eb5c97456ac06b3d7107de20ce
Suman Barick <codotronix@gmail.com>
2020-09-26 21:11:14 +0530

    Updates

---
+++ precache-manifest.7ad78da668870a7909b2592de89d7989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1bac202f51af4ba5d2b106f696b4a5a",
+    "url": "/snake-n-snack/index.html"
+  },
+  {
+    "revision": "650ed23a5e032d2a473e",
+    "url": "/snake-n-snack/static/css/main.5c7b62bb.chunk.css"
+  },
+  {
+    "revision": "0209223944613b074995",
+    "url": "/snake-n-snack/static/js/2.063134d5.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/snake-n-snack/static/js/2.063134d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650ed23a5e032d2a473e",
+    "url": "/snake-n-snack/static/js/main.36fa6629.chunk.js"
+  },
+  {
+    "revision": "48838eba0db1eeb12b7d",
+    "url": "/snake-n-snack/static/js/runtime-main.83c0fdb9.js"
+  }
+]);
